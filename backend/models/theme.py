8f9f920c1918d8fde6b3d57899668f81ed40c43e
Maja49 <maja.pavic02@gmail.com>
2024-11-25 21:08:26 +0100

    add theme model

---
+++ backend/models/theme.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Theme(db.Model):
+    _tablename_ = 'themes'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text)
+
