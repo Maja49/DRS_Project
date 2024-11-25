db8b613c17bba3607d63a12529e48e6708cb5d9e
Maja49 <maja.pavic02@gmail.com>
2024-11-25 20:15:09 +0100

    password for dataBase

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,6 @@
 #da bismo se povezali sa bazom uz pomoc SQLAlchemy
 DB_HOST = 'localhost'
 DB_USER = 'root'  # Zameniti svojim korisničkim imenom
-DB_PASSWORD = 'maja123'  # Zameniti svojom šifrom
+DB_PASSWORD = '2310'  # Zameniti svojom šifrom
 DB_NAME = 'drs'  # Naziv tvoje baze
 SECRET_KEY = 'a#4h!r3d89D09$2faHf!sd83F@#s'  # Tajni ključ za Flask aplikaciju
