622d6694ffe1ed6c862227bb6564c987a991db5a
NatalijaCelic <natalijacelicc2002@gmail.com>
2024-11-25 19:53:44 +0100

    database update
    made database folder and file

---
+++ backend/database/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE User (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    adress VARCHAR(100) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    country VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(15),
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    is_admin BOOLEAN NOT NULL,
+    is_approved BOOLEAN NOT NULL
+);
+
+INSERT INTO User (name, lastname, adress, city, country, phone_number, email, password, username, is_admin)
+VALUES 
+('Maja', 'Pavić', 'Neka Adresa 1', 'Novi Sad', 'Serbia', '1234567890', 'maja2@example.com', 'primer', 'primer', FALSE);
+
+ALTER TABLE User ADD COLUMN is_approved BOOLEAN DEFAULT FALSE;
