9c9dc122b41f615d7e1e68034e23e99e639ef9e7
NatalijaCelic <natalijacelicc2002@gmail.com>
2024-11-25 19:59:09 +0100

    adding utils for tokens

---
+++ backend/utils/token_utils.py
@@ -0,0 +1,25 @@
+#ovdje definisemo nas JWT
+import jwt
+from datetime import datetime, timedelta, timezone
+
+SECRET_KEY = "a#4h!r3d89D09$2faHf!sd83F@#s"
+
+# Funkcija za generisanje tokena
+def generate_token(user_id, is_admin):
+    payload = {
+        "user_id": user_id,
+        "is_admin": is_admin,
+        "exp": datetime.now(timezone.utc) + timedelta(hours=1)  # Token važi 1 sat
+    }
+    return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+
+# Funkcija za validaciju tokena
+def decode_token(token):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        return payload  # Ako je validan, vraća podatke iz tokena
+    except jwt.ExpiredSignatureError:
+        return {"error": "Token expired"}
+    except jwt.InvalidTokenError:
+        return {"error": "Invalid token"}
+
