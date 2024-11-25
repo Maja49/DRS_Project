be5485892f4c391cfa342f248af5acc49d578779
Maja49 <maja.pavic02@gmail.com>
2024-11-25 20:52:57 +0100

    izmena

--- DRS.session.sql
+++ DRS.session.sql
@@ -1,1 +1,12 @@
-SELECT * FROM user;
+select * from user;
+
+SELECT * from Theme;
+
+CREATE TABLE Theme(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    id2 VARCHAR(255) NOT NULL,
+    description VARCHAR(255)
+)
+
+select * from Theme;
