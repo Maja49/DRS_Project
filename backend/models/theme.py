fa471dcd32fdab54e1f267bddc79ba635816aecf
Maja49 <maja.pavic02@gmail.com>
2024-11-26 16:42:50 +0100

    izmena

--- backend/models/theme.py
+++ backend/models/theme.py
@@ -5,6 +5,8 @@ db = SQLAlchemy()
 class Theme(db.Model):
     __tablename__ = 'theme'
 
+    #proba
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False, unique=True)  # Naziv teme, mora biti jedinstven
     description = db.Column(db.Text, nullable=True)  # Opis teme, može biti prazan
