fda9e16da015c8784c8c3001259414722268a05f
aleksandar01st <aco01.stankovic@gmail.com>
2024-12-16 22:45:35 +0100

    adding discussion in admin page
    adding discussion in admin page

--- frontend/src/pages/AdminPage.tsx
+++ frontend/src/pages/AdminPage.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./AdminPage.css";
+import "./Discussion";
+import { DiscussionProps } from "./Discussion";
+import { Discussion } from "./Discussion";
 
 interface RegistrationRequest {
   id: number;
@@ -39,6 +42,11 @@ const AdminPage: React.FC = () => {
   const [name, setName] = useState<string>("");
   const [description, setDescription] = useState<string>("");
   const [successMessage, setSuccessMessage] = useState<string>("");
+  const [discussions, setDiscussions] = useState<DiscussionProps[]>([]);
+  const [isAddPostModalVisible, setIsAddPostModalVisible] = useState(false);
+  const [newPostTitle, setNewPostTitle] = useState("");
+  const [newPostText, setNewPostText] = useState("");
+  const [newPostTheme, setNewPostTheme] = useState("");
 
   function getUsernameFromToken(): string {
     const token = localStorage.getItem("auth_token"); // JWT token iz localStorage
@@ -163,13 +171,41 @@ const AdminPage: React.FC = () => {
   useEffect(() => {
     fetchRegistrationRequests();
     fetchUsers();
-    fetchThemes(); // Dohvatanje tema
   }, []);
 
-  const handleSearch = () => {
-    fetch(`/api/discussions/search?query=${searchQuery}`)
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+  const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault(); // Prevents page refresh
+    console.log("Search started with query:", searchQuery);
+    if (searchQuery.trim()) {
+      fetch(
+        `http://localhost:5000/api/discussion/search?theme_name=${searchQuery}`
+      )
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log("Search Results:", data);
+          setDiscussions(data); // Sets discussions to the search results
+        })
+        .catch((error) => {
+          console.error("Error fetching search results:", error);
+        });
+    } else {
+      console.log("Search query is empty");
+      fetch("http://localhost:5000/api/discussion/get_all")
+        .then((response) => response.json())
+        .then((data) => setDiscussions(data.discussions))
+        .catch((error) => console.error("Error fetching discussions:", error));
+    }
+  };
+
+  const handleSearchKeyPress = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      handleSearch(e);
+    }
   };
 
   const handleLogout = () => {
@@ -181,8 +217,96 @@ const AdminPage: React.FC = () => {
     window.location.href = "/user";
   };
 
-  ////////teme///////////
-  // Dohvatanje svih tema
+  ////////teme i duskusije///////////
+  useEffect(() => {
+    // Učitavamo sve teme sa servera
+    fetch("http://localhost:5000/api/discussion/themes")
+      .then((response) => response.json())
+      .then((data) => setThemes(data)) // Postavljamo teme u state
+      .catch((error) => console.error("Error fetching themes:", error));
+
+    fetch("http://localhost:5000/api/discussion/get_all")
+      .then((response) => response.json())
+      .then((data) => setDiscussions(data.discussions))
+      .catch((error) => console.error("Error fetching discussions:", error));
+  }, []);
+
+  const handleDiscussions = () => {
+    window.location.href = "/Discussions"; // Redirect to discussions page
+  };
+
+  const handleAddPost = () => {
+    console.log();
+    setIsAddPostModalVisible(true);
+  };
+
+  const handleSavePost = () => {
+    if (newPostText.trim() && newPostTheme.trim()) {
+      const newDiscussion = {
+        title: newPostTitle,
+        text: newPostText,
+        theme_name: newPostTheme,
+      };
+
+      const userToken = localStorage.getItem("auth_token");
+
+      if (!userToken) {
+        console.error("User token is not available!");
+        return;
+      }
+
+      fetch("http://localhost:5000/api/discussion/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userToken}`,
+        },
+        body: JSON.stringify(newDiscussion),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("New discussion added:", data);
+          setDiscussions([data.discussion, ...discussions]);
+          setIsAddPostModalVisible(false);
+          setNewPostTitle("");
+          setNewPostText("");
+          setNewPostTheme(""); // Resetovanje teme
+        })
+        .catch((error) => console.error("Error adding discussion:", error));
+    } else {
+      alert("Please fill in all fields!");
+    }
+  };
+
+  const handleDelete = async (themeId: number) => {
+    const token = localStorage.getItem("auth_token");
+    if (!token) {
+      setErrorMessage("Unauthorized: No token found.");
+      return;
+    }
+
+    try {
+      console.log(themeId);
+      const response = await axios.delete(
+        `http://localhost:5000/api/admin/theme-delete/${themeId}`, // API endpoint za brisanje teme
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      // Uklanjanje teme iz liste nakon uspešnog brisanja
+      setThemes((prevThemes) =>
+        prevThemes.filter((theme) => theme.id !== themeId)
+      );
+      setSuccessMessage(response.data.message); // Prikazivanje poruke o uspehu
+    } catch (error) {
+      console.error("Error deleting theme:", error);
+      setErrorMessage("Error deleting theme.");
+    }
+  };
+
   const fetchThemes = async () => {
     const token = localStorage.getItem("auth_token");
     if (!token) {
@@ -206,7 +330,6 @@ const AdminPage: React.FC = () => {
     }
   };
 
-  // Funkcija za slanje POST zahteva za kreiranje nove teme
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
@@ -243,41 +366,17 @@ const AdminPage: React.FC = () => {
     }
   };
 
-  const handleDelete = async (themeId: number) => {
-    const token = localStorage.getItem("auth_token");
-    if (!token) {
-      setErrorMessage("Unauthorized: No token found.");
-      return;
-    }
-
-    try {
-      console.log(themeId);
-      const response = await axios.delete(
-        `http://localhost:5000/api/admin/theme-delete/${themeId}`, // API endpoint za brisanje teme
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-
-      // Uklanjanje teme iz liste nakon uspešnog brisanja
-      setThemes((prevThemes) =>
-        prevThemes.filter((theme) => theme.id !== themeId)
-      );
-      setSuccessMessage(response.data.message); // Prikazivanje poruke o uspehu
-    } catch (error) {
-      console.error("Error deleting theme:", error);
-      setErrorMessage("Error deleting theme.");
-    }
-  };
-
   return (
     <div>
       <nav className="navbar">
         <div className="navbar-left">
           <img src="/icon.png" alt="Logo" className="logo" />
-          <h1 className="app-name">Chatify</h1>
+          <h1
+            className="app-name"
+            onClick={() => (window.location.href = "/admin")}
+          >
+            Chatify
+          </h1>
         </div>
         <div className="navbar-center">
           <div className="search-container">
@@ -286,23 +385,65 @@ const AdminPage: React.FC = () => {
               placeholder="Search..."
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)}
-              onBlur={handleSearch}
+              onKeyDown={handleSearchKeyPress}
               className="search-input"
             />
             <img src="/search.png" alt="Search Icon" className="search-icon" />
           </div>
         </div>
         <div className="navbar-right">
+          <button className="add-post-button" onClick={handleAddPost}>
+            Add post +
+          </button>
           <div
             className="profile-section"
             onClick={() => setDropdownVisible(!dropdownVisible)}
           >
-            <img src="/profile.png" alt="Profile" className="profile-icon" />
             <span className="username">{username}</span>
+            <img src="/profile.png" alt="Profile" className="profile-icon" />
             <div className={`dropdown-menu ${dropdownVisible ? "active" : ""}`}>
+              <button onClick={handleDiscussions}>My discussions</button>
               <button onClick={handleEdit}>Edit Profile</button>
               <button onClick={handleLogout}>Logout</button>
             </div>
+            {isAddPostModalVisible && (
+              <div className="modal">
+                <div className="add-post-container">
+                  <h2>Add New Discussion</h2>
+                  <input
+                    type="text"
+                    placeholder="Title"
+                    value={newPostTitle}
+                    onChange={(e) => setNewPostTitle(e.target.value)}
+                    className="add-post-title"
+                  />
+                  <textarea
+                    placeholder="Text"
+                    value={newPostText}
+                    onChange={(e) => setNewPostText(e.target.value)}
+                    className="add-post-text"
+                  />
+                  <select
+                    value={newPostTheme}
+                    onChange={(e) => setNewPostTheme(e.target.value)}
+                    className="add-post-theme"
+                  >
+                    <option value="">Select Theme</option>
+                    {themes.map((theme) => (
+                      <option key={theme.id} value={theme.name}>
+                        {theme.name}
+                      </option>
+                    ))}
+                  </select>
+                  <div className="add-post-actions">
+                    <button onClick={handleSavePost}>Save</button>
+                    <button onClick={() => setIsAddPostModalVisible(false)}>
+                      Cancel
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </nav>
@@ -327,9 +468,30 @@ const AdminPage: React.FC = () => {
           >
             Themes
           </button>
+          <button
+            className={activeTab === "discussion" ? "active" : ""}
+            onClick={() => setActiveTab("discussion")}
+          >
+            Discussions
+          </button>
         </div>
 
         {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+        {activeTab === "discussion" && (
+          <div className="discussion-space">
+            {/* Discussions Section */}
+            <div className="discussions-section">
+              {discussions.length > 0 ? (
+                discussions.map((discussion) => (
+                  <Discussion key={discussion.id} {...discussion} />
+                ))
+              ) : (
+                <p>No discussions available</p>
+              )}
+            </div>
+          </div>
+        )}
 
         {activeTab === "themes" && (
           <>
--- frontend/src/pages/Discussion.tsx
+++ frontend/src/pages/Discussion.tsx
@@ -1,20 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { formatDistanceToNow } from "date-fns"; // instalirajte ovo
-import "./Home.css";
+import "./Discussion.css";
 
-interface User {
-  username: string;
-}
-
-interface Comment {
-  id: number;
-  user_id: number;
-  text: string;
-  mentioned_user_id?: number | null;
-  discussion_id: number;
-}
-
-interface DiscussionProps {
+export interface DiscussionProps {
   id: number;
   text: string;
   title: string;
@@ -26,13 +14,13 @@ interface DiscussionProps {
   user_id: number;
 }
 
-const Discussion: React.FC<DiscussionProps> = ({
+export const Discussion: React.FC<DiscussionProps> = ({
   id,
   text,
   title,
   theme_name,
   created_at,
-  updated_at,
+  // updated_at,
   likes: initialLikes,
   dislikes: initialDislikes,
   user_id,
@@ -42,63 +30,79 @@ const Discussion: React.FC<DiscussionProps> = ({
   const [hasLiked, setHasLiked] = useState<boolean>(false);
   const [hasDisliked, setHasDisliked] = useState<boolean>(false);
   const [user, setUser] = useState<{ username: string } | null>(null);
-  const [users, setUsers] = useState<string[]>([]);
 
-  const [comments, setComments] = useState<Comment[]>([]);
-  const [newComment, setNewComment] = useState<Comment>({
-    id: 0,
-    user_id: 0,
-    text: "",
-    mentioned_user_id: null,
-    discussion_id: 0,
-  });
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [editedText, setEditedText] = useState<string>(text);
+  const [editedTitle, setEditedTitle] = useState<string>(title);
+  const [showDeleteConfirmation, setShowDeleteConfirmation] =
+    useState<boolean>(false);
 
+  const [comments, setComments] = useState<string[]>([]);
+  const [newComment, setNewComment] = useState<string>("");
   const [isCommentSectionVisible, setIsCommentSectionVisible] =
     useState<boolean>(false);
 
-  const getUserById = (userId: number | string): Promise<string> => {
-    return fetch(`http://localhost:5000/api/user/get_user/${userId}`)
-      .then((response) => response.json())
-      .then((data: User) => data.username)
-      .catch((error) => {
-        console.error("Error fetching user data:", error);
-        return "Unknown User";
-      });
-  };
-
-  // Efekat za učitavanje korisničkih podataka
-  useEffect(() => {
-    const fetchUsers = async () => {
-      // Mapiramo sve user_id iz komentara da bismo ih dobili sa API-a
-      const userPromises = comments.map(
-        (comment) => getUserById(comment.user_id) // user_id može biti broj ili string
-      );
-      const usersData = await Promise.all(userPromises);
-      setUsers(usersData); // Postavljamo korisnička imena
-    };
-
-    fetchUsers();
-  }, [comments]); // Učitava kada se komentari promene
   useEffect(() => {
     // Fetch user details based on user_id
+    console.log("Fetching data for user_id:", user_id);
     fetch(`http://localhost:5000/api/user/get_user/${user_id}`)
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
       .then((data) => setUser(data))
       .catch((error) => console.error("Error fetching user data:", error));
+  }, [user_id]);
 
-    // Fetch comments for this discussion when comment section is visible
-    if (isCommentSectionVisible) {
-      fetch(`http://localhost:5000/api/comment/getcomments//${id}`)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-          }
-          return response.json();
-        })
-        .then((data) => setComments(data))
-        .catch((error) => console.error("Error fetching comments:", error));
-    }
-  }, [isCommentSectionVisible, id, user_id]);
+  const handleDelete = () => {
+    console.log("Fetching data for id diss:", id);
+
+    const token = localStorage.getItem("auth_token");
+
+    fetch(`http://localhost:5000/api/discussion/delete/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log("Discussion deleted successfully");
+          window.location.reload(); // Reload discussions
+        } else {
+          console.error("Error deleting discussion");
+        }
+      })
+      .catch((error) => console.error("Error deleting discussion:", error));
+  };
+
+  const handleSaveEdit = () => {
+    console.log("Fetching data for id diss:", id);
+
+    const token = localStorage.getItem("auth_token"); // Retrieving the token from localStorage
+
+    fetch(`http://localhost:5000/api/discussion/update/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`, // Including the token in the Authorization header
+      },
+      body: JSON.stringify({ title: editedTitle, text: editedText }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          setIsEditing(false);
+          console.log("Discussion updated successfully");
+          window.location.reload(); // Refresh
+        } else {
+          console.error("Error updating discussion");
+        }
+      })
+      .catch((error) => console.error("Error updating discussion:", error));
+  };
 
   const handleLike = () => {
     if (!hasLiked) {
@@ -129,55 +133,18 @@ const Discussion: React.FC<DiscussionProps> = ({
   };
 
   const handleCommentChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNewComment((prevComment) => ({
-      ...prevComment,
-      text: e.target.value,
-    }));
+    setNewComment(e.target.value);
   };
 
-  const handleAddComment: React.MouseEventHandler<HTMLButtonElement> = async (
-    e
-  ) => {
-    e.preventDefault();
-    const discussionId = e.currentTarget.dataset.id;
-    if (discussionId && newComment.text.trim() !== "") {
-      try {
-        const response = await fetch(
-          `http://localhost:5000/api/comment/comment/${discussionId}`,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${localStorage.getItem("auth_token")}`,
-            },
-            body: JSON.stringify({
-              text: newComment.text,
-              discussion_id: discussionId,
-            }),
-          }
-        );
-        const data = await response.json();
-
-        if (response.ok) {
-          setComments([...comments, data]); // Add the new comment to the comments list
-          setNewComment({ ...newComment, text: "" }); // Clear the input field
-        } else {
-          console.error("Error adding comment:", data.message);
-        }
-      } catch (error) {
-        console.error("Error posting comment:", error);
-      }
+  const handleAddComment = () => {
+    if (newComment.trim() !== "") {
+      setComments([...comments, newComment]);
+      setNewComment("");
     }
   };
 
   const handleCancelComment = () => {
-    setNewComment({
-      id: 0,
-      user_id: 0,
-      text: "",
-      mentioned_user_id: null,
-      discussion_id: 0,
-    });
+    setNewComment(""); // Clears the input field
   };
 
   const formattedTime = created_at
@@ -186,82 +153,102 @@ const Discussion: React.FC<DiscussionProps> = ({
 
   return (
     <div className="discussion-card">
-      <div className="discussion-header">
-        <p className="topic">{theme_name}</p>
-        <p className="discussion-created">{formattedTime}</p>
-      </div>
-      <p className="user">posted by: {user?.username}</p>
-      <p className="discussion-title">{title}</p>
-      <div className="discussion-text">{text}</div>
-      <div className="discussion-actions">
-        <button
-          className={`like-button ${hasLiked ? "active" : ""}`}
-          onClick={handleLike}
-        >
-          ❤️ {likes}
-        </button>
-        <button
-          className={`dislike-button ${hasDisliked ? "active" : ""}`}
-          onClick={handleDislike}
-        >
-          💔 {dislikes}
-        </button>
-        <button
-          className="comment-button"
-          onClick={() => setIsCommentSectionVisible(!isCommentSectionVisible)}
-        >
-          💬
-        </button>
-      </div>
+      {!isEditing ? (
+        <>
+          <div className="discussion-header">
+            <p className="topic">{theme_name}</p>
+            <button onClick={() => setShowDeleteConfirmation(true)}>
+              🗑️ Delete
+            </button>
+            <button onClick={() => setIsEditing(true)}>✏️ Edit</button>
+            <p className="discussion-created">{formattedTime}</p>
+          </div>
+          <p className="user">posted by: {user?.username}</p>
+          <p className="discussion-title">{title}</p>
+          <div className="discussion-text">{text}</div>
+          <div className="discussion-actions">
+            <button
+              className={`like-button ${hasLiked ? "active" : ""}`}
+              onClick={handleLike}
+            >
+              ❤️ {likes}
+            </button>
+            <button
+              className={`dislike-button ${hasDisliked ? "active" : ""}`}
+              onClick={handleDislike}
+            >
+              💔 {dislikes}
+            </button>
+            <button
+              className="comment-button"
+              onClick={() =>
+                setIsCommentSectionVisible(!isCommentSectionVisible)
+              }
+            >
+              💬
+            </button>
+          </div>
+        </>
+      ) : (
+        <div className="edit-discussion">
+          <input
+            type="text"
+            value={editedTitle}
+            onChange={(e) => setEditedTitle(e.target.value)}
+            placeholder="Edit title"
+          />
+          <textarea
+            value={editedText}
+            onChange={(e) => setEditedText(e.target.value)}
+            placeholder="Edit text"
+          />
+          <button onClick={handleSaveEdit}>Save</button>
+          <button onClick={() => setIsEditing(false)}>Cancel</button>
+        </div>
+      )}
+
+      {showDeleteConfirmation && (
+        <div className="delete-confirmation">
+          <p>Are you sure you want to delete this discussion?</p>
+          <button onClick={handleDelete}>Yes</button>
+          <button onClick={() => setShowDeleteConfirmation(false)}>No</button>
+        </div>
+      )}
 
       <div className="comment-section">
         {isCommentSectionVisible && (
-          <div>
-            {/* Comment input */}
-            <div className="comment-input-container">
-              <input
-                type="text"
-                placeholder={newComment.text === "" ? "Add Comment..." : ""}
-                value={newComment.text}
-                onChange={handleCommentChange}
-              />
-              <div className="comment-buttons">
-                <button className="cancel-button" onClick={handleCancelComment}>
-                  Cancel
-                </button>
-                <button
-                  className="add-comment-button"
-                  data-id={id}
-                  onClick={handleAddComment}
-                >
-                  Add Comment
-                </button>
-              </div>
-            </div>
-
-            {/* List of comments */}
-            <div className="comments-list">
-              {comments.length > 0 ? (
-                comments.map((comment, index) => (
-                  <div key={comment.id} className="comment">
-                    <p>
-                      <strong>{users[index] || "loading.."}</strong>:{" "}
-                      {comment.text}
-                    </p>
-                  </div>
-                ))
-              ) : (
-                <p>No comments yet. Be the first to comment!</p>
-              )}
+          <div className="comment-input-container">
+            <input
+              type="text"
+              placeholder={newComment === "" ? "Add Comment..." : ""}
+              value={newComment}
+              onChange={handleCommentChange}
+              onFocus={() => {}}
+            />
+            <div className="comment-buttons">
+              <button className="cancel-button" onClick={handleCancelComment}>
+                Cancel
+              </button>
+              <button className="add-comment-button" onClick={handleAddComment}>
+                Comment
+              </button>
             </div>
           </div>
         )}
+
+        <div className="comments-list">
+          {comments.map((comment, index) => (
+            <div key={index} className="comment">
+              {comment}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
-const Home: React.FC = () => {
+const Discussions: React.FC = () => {
   //const username = localStorage.getItem("user_id") || "Guest";
   const username = getUsernameFromToken();
 
@@ -283,11 +270,13 @@ const Home: React.FC = () => {
         return decodedPayload.username || "Guest"; // Vraćamo korisničko ime ili "Guest" ako nije dostupno
       } catch (error) {
         console.error("Error decoding token:", error);
-        return "Guest"; // Ako nešto pođe po zlu, vraćamo "Guest"
+        return "Guest";
       }
     }
     return "Guest"; // Ako token ne postoji
   }
+
+  const user_id = localStorage.getItem("user_id");
 
   useEffect(() => {
     // Učitavamo sve teme sa servera
@@ -296,11 +285,11 @@ const Home: React.FC = () => {
       .then((data) => setThemes(data)) // Postavljamo teme u state
       .catch((error) => console.error("Error fetching themes:", error));
 
-    fetch("http://localhost:5000/api/discussion/get_all")
+    fetch(`http://localhost:5000/api/discussion/get_by_user/${user_id}`)
       .then((response) => response.json())
       .then((data) => setDiscussions(data.discussions))
       .catch((error) => console.error("Error fetching discussions:", error));
-  }, []);
+  }, [user_id]);
 
   /*useEffect(() => {
     fetch("http://localhost:5000/api/discussion/get_all")
@@ -410,12 +399,23 @@ const Home: React.FC = () => {
     }
   };
 
+  const handleLogoClick = () => {
+    window.location.href = "/Home"; // Redirect to home page
+  };
+
   return (
     <div>
       <nav className="navbar">
         <div className="navbar-left">
-          <img src="/icon.png" alt="Logo" className="logo" />
-          <h1 className="app-name">Chatify</h1>
+          <img
+            src="/icon.png"
+            alt="Logo"
+            className="logo"
+            onClick={handleLogoClick}
+          />
+          <h1 className="app-name" onClick={handleLogoClick}>
+            Chatify
+          </h1>
         </div>
         <div className="navbar-center">
           <div className="search-container">
@@ -502,4 +502,4 @@ const Home: React.FC = () => {
   );
 };
 
-export default Home;
+export default Discussions;
