954ef722733bfc5c62bb1144c2788ed936f686d6
Maja49 <maja.pavic02@gmail.com>
2025-07-07 21:53:16 +0200

    update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-/*
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})*/
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',  // ⚠️ OVO JE KLJUČNO da se vidi iz Dockera
-    port: 5173
+    port: 5173,
+    allowedHosts: [
+      'drs-frontend-b2bt.onrender.com'
+    ],
   },
-    preview: {
+  preview: {
     host: true,
     port: 4173,
     allowedHosts: [
